728d55349a04fca57b6b3ad7e6e29be358d8bdc7
LiuYe <liuyehcf@163.com>
2017-03-08 23:50:57 +0800

    2017_03_08

--- commit.sh
+++ commit.sh
@@ -1,10 +1,7 @@
-#£¡/bin/bash
-
-
-description=$(date +%Y)"_"$(date +%m)"_"$(date +%d)
+# description=$(date +%Y)"_"$(date +%m)"_"$(date +%d)
 
 git add .
 
-git commit -m $description
+# git commit -m ${description}
 
 git push origin master:master
